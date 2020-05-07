685714c87e0cd91b88b6a250a3bdabc7995a291e
csahithi <chigurupati.sahithi@gmail.com>
2020-05-07 15:38:54 +0530

    restaurant onboarding

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant/screens/welcome_screen.dart';
+import 'package:restaurant/screens/login_screen.dart';
+import 'package:restaurant/screens/registration_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+      },
+    );
+  }
+}
